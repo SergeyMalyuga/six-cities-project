e0e10eae0051ea5d9e94b46b2e7df1816a2e0b9b
Sergey Malyuga <sergey@malyuga.ru>
2025-10-02 11:51:43 +0300

    Создать компоненты comment-list, comment-item. Создать сервис comment-service.

---
+++ src/app/core/services/comment-api.service.ts
@@ -0,0 +1,22 @@
+import {inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Comment} from '../models/comments';
+import {APIRoute, BASE_URL} from '../constants/const';
+
+@Injectable(
+  {
+    providedIn: 'root',
+  }
+)
+export class CommentApiService {
+  private http: HttpClient = inject(HttpClient);
+
+  public getComments(): Observable<Comment[]> {
+    return this.http.get<Comment[]>(`${BASE_URL}/${APIRoute.COMMENTS}`)
+  }
+
+  public postComment(offerId: string): Observable<Comment> {
+    return this.http.post<Comment>(`${BASE_URL}/${APIRoute.COMMENTS}/${offerId}`, {})
+  }
+}
---
+++ src/app/features/comment-item/comment-item.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-comment-item',
+  templateUrl: './comment-item.component.html'
+})
+export class CommentItemComponent {
+  
+}
