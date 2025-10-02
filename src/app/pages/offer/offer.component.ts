84723aaa055743b60665950a493a7caa0c9ab631
Sergey Malyuga <sergey@malyuga.ru>
2025-10-02 11:34:03 +0300

    Реализовать загрузку оффера по id при переходе на страницу offer. Передать данные из загруженного офеера в шаблон компонента Offer.

--- src/app/pages/offer/offer.component.ts
+++ src/app/pages/offer/offer.component.ts
@@ -1,13 +1,36 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, effect, inject, signal, WritableSignal} from '@angular/core';
 import {HeaderComponent} from '../../shared/header/header.component';
+import {Offer} from '../../core/models/offers';
+import {ActivatedRoute} from '@angular/router';
+import {OfferApiService} from '../../core/services/offer-api.service';
+import {Subject, takeUntil} from 'rxjs';
+import {CapitalizePipe} from '../../shared/pipes/capitalize.pipe';
 
 @Component({
   selector: 'app-offer',
   imports: [
-    HeaderComponent
+    HeaderComponent,
+    CapitalizePipe
   ],
   templateUrl: './offer.component.html',
   styleUrl: './offer.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OfferComponent {}
+export class OfferComponent {
+  public offer: WritableSignal<Offer | undefined> = signal<Offer | undefined>(undefined);
+
+  private offerId: WritableSignal<string | null> = signal<string | null>(null);
+  private activeRoute = inject(ActivatedRoute);
+  private offerApiService: OfferApiService = inject(OfferApiService);
+  private destroySubject: Subject<void> = new Subject<void>();
+
+  constructor() {
+    this.activeRoute.paramMap.subscribe(params => this.offerId.set(params.get('id')));
+    effect(() => {
+      const id = this.offerId();
+      if (id) {
+        this.offerApiService.getOfferById(id).pipe(takeUntil(this.destroySubject)).subscribe(offer => this.offer.set(offer))
+      }
+    })
+  }
+}
--- src/app/shared/pipes/capitalize.pipe.ts
+++ src/app/shared/pipes/capitalize.pipe.ts
@@ -4,7 +4,10 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'capitalize',
 })
 export class CapitalizePipe implements PipeTransform {
-  transform(value: string) {
-    return value.replace(value.charAt(0), value.charAt(0).toUpperCase());
+  transform(value: string | undefined) {
+    if (value) {
+      return value.replace(value.charAt(0), value.charAt(0).toUpperCase());
+    }
+    return '';
   }
 }
